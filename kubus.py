13af793a70e2a16ff72eb70d059678a4f6640c01
Sitisarah375 <sitisarahaurarozak@smkwikrama.sch.id>
2023-03-09 16:20:20 +0700

    sarah

--- kubus.py
+++ kubus.py
@@ -1,4 +1,10 @@
+
 rusuk = int(input("Rusuk  : "))
 
 print("Luas = ", 6*(rusuk**2))
 print("volume = ", rusuk*rusuk*rusuk)
+
+belanja = int(input("Belanja :"))
+belanja = int(input("Belanja :"))
+bayar = int-(int*int/100)
+print ("")
